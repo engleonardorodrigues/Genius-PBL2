4167bf3b773ef0fff8c117d1e284cdfe10bcd1b5
Leonardo Rodrigues <leonardo.rodrigues7@yahoo.com.br>
2025-05-12 11:25:43 -0300

    Add s3 sequence flags for count sequence size and add player simulation input for test

--- software/lab_genius_asm.s
+++ software/lab_genius_asm.s
@@ -11,48 +11,52 @@
 
 genius_game:
 
-ST_IDLE:                      # Estado inicial de configurações etc
+ST_IDLE:                             # Estado inicial de configurações etc
 
-    li   s0,  0b0000          # Modo = Siga (bit0 = 0), Velocidade = Lenta (bit1 = 0) Dificuldade = (bit2 e 3 = 0)  
+    li   s0,  0b0000                 # Modo = Siga (bit0 = 0), Velocidade = Lenta (bit1 = 0) Dificuldade = (bit2 e 3 = 0)  
     li   t0,  START_BIT
     
-    beqz t0,  ST_GEN          # Se start pressionado, vai para o estado de gerar a sequência
-    j         ST_IDLE         # Se não, continua no estado de idle
+    beqz t0,  ST_GEN                 # Se start pressionado, vai para o estado de gerar a sequência
+    j         ST_IDLE                # Se não, continua no estado de idle
 
 ST_GEN:
 
-    li     s1, 0            # s1: led_sequence_reg1 vai armazenar os 16 bits gerados (cores
-    li     t3, 0            # contador de quantas iterações já fizemos
-    li     s4, 8            # s4: Sequence_size (define o tamanho da sequência de acordo com o nível de dificuldade)	
+    li     s1, 0                     # s1: led_sequence_reg1 vai armazenar os 16 bits gerados (cores
+    li     t3, 0                     # contador de quantas iterações já fizemos
+    li     s4, 8                     # s4: Sequence_size (define o tamanho da sequência de acordo com o nível de dificuldade)	
 
 prng_generation:
     
     # Gera um número (aleatório) para inserir na sequência
-    addi   t1, t1, 3                # Seed temporária (substituir por lógica adequada)
-    andi   t2, t1, 0b11             # Mascara para obter 2 bits (0-3)
+    addi   t1, t1, 3 #(Seed)         # Seed temporária (substituir por lógica adequada)
+    andi   t2, t1, 0b11              # Mascara para obter 2 bits (0-3)
 
-    slli   t4, t3, 1                # Desloca os dois bits para esquerda
-    sll    t5, t2, t4               # Desloca os bits da nova cor para a posição atual
-    or     s1, s1, t5               # Adiciona ao registrador s1
+    slli   t4, t3, 1                 # Desloca os dois bits para esquerda
+    sll    t5, t2, t4                # Desloca os bits da nova cor para a posição atual
+    or     s1, s1, t5                # Adiciona ao registrador s1
     addi   t3, t3, 1  
     
-    j      ST_SHOW_LEDS             # Vai mostrar o LED atual
+    j      ST_SHOW_LEDS              # Vai mostrar o LED atual
 
-inc_prng_sec:
-    # incrementa contagem de geração aleatória
-    blt    t3, s4, prng_generation    # s4 tamanho da sequência (8 fácil, 16 médio, 32 dificil)
+aval_sec_size:                        # incrementa contagem de geração aleatória
     
+    blt    t3, s4, prng_generation   # Avalia se sequência foi concluida (8 fácil, 16 médio, 32 dificil)
+    j      VICTORY
+
 ST_SHOW_LEDS:
     
-    bnez s3, show_color            # Necessário para manter somente 1 bit enable acionado por vez
-    addi s3, s3, 1                 # Aciona o bit enable para ser possível visualizar o LED no jogo
+    # bnez s3, show_color              # Necessário para manter somente 1 bit enable acionado por vez
+    # addi s3, s3, 1                   # Aciona o bit enable para ser possível visualizar o LED no jogo
 
 show_color:
     
     # Extrai os 2 bits da cor atual (t2) e mapeia para 4 bits em s7
-    li   s7, 0                   # Reseta s7
     
+    li   s7, 0                   # Reseta s7 (Apaga todos os leds)
+    li   s6, 0                   # Desabilita pino (Enable LED)
+
     # Verifica qual cor está em t2 e configura os bits correspondentes
+    
     beqz t2,     set_blue        # Se cor = 00 (Blue)
     li   t5, 1
     beq  t2, t5, set_green       # Se cor = 01 (Green)
@@ -62,32 +66,102 @@ show_color:
     beq  t2, t5, set_red         # Se cor = 11 (Red)
     
 set_green:
-    ori  s7, s7, 0b0001          # Verde    [1:0] = 01 -> saída 0001 (bit 0)
-    # j    end_mapping
-      j  ST_PLAYER_INPUT
+    ori  s7, s7, 0b0001          # Verde      [1:0] = 01 -> saída 0001 (bit 0)
+    li   s6, 1                   # Enable_Led
+
+    slli s3, s3, 1               # Rotação a esquerda de um bit
+    addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
+
+    j    ST_PLAYER_INPUT
+
 set_blue:
-    ori  s7, s7, 0b0010          # Azul     [2:1] = 00 -> saída 0010 (bit 1)
-    # j    end_mapping
-       j  ST_PLAYER_INPUT
+    ori  s7, s7, 0b0010          # Azul       [2:1] = 00 -> saída 0010 (bit 1)
+    li   s6, 1                   # Enable_Led
+
+    slli s3, s3, 1               # Rotação a esquerda de um bit
+    addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
+
+    j    ST_PLAYER_INPUT
+
 set_red:
-    ori  s7, s7, 0b0100          # Vermelho [3:2] = 11 -> saída 0100 (bit 2)
-    # j    end_mapping
-      j  ST_PLAYER_INPUT
+    ori  s7, s7, 0b0100          # Vermelho   [3:2] = 11 -> saída 0100 (bit 2)
+    li   s6, 1                   # Enable_Led
+
+    slli s3, s3, 1               # Rotação a esquerda de um bit
+    addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
+
+    j    ST_PLAYER_INPUT
+
 set_yellow:
-    ori  s7, s7, 0b1000          # Amarelo  [4:3] = 10 -> saída 1000 (bit 3)
-    # j    end_mapping
-      j  ST_PLAYER_INPUT
+    ori  s7, s7, 0b1000          # Amarelo    [4:3] = 10 -> saída 1000 (bit 3)
+    li   s6, 1                   # Enable_Led
+
+    #andi s6, s6, delay_led
+
+    slli s3, s3, 1               # Rotação a esquerda de um bit
+    addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
+
+    j    ST_PLAYER_INPUT
+
+
+#delay_led:                       # ADAPTAR O TEMPO DO LED DE ACORDO COM A VELOCIDADE (2s lento, 1s rápido)
+    
+#    li t1, 2000
+
+#    addi t0, t0, 1  
+#    blt  t0, t1, delay_led 
 
 
 ST_PLAYER_INPUT:
 
-    li    t5, 0b11                    # Simula apertar o vermelho (11) - CORRETO 
+    # Simula a entrada com base no contador s2 (0-3)
+    beqz  s5, input_red              # Jogada 1: Vermelho (11)
+    li    t0, 1
+    beq   s5, t0, input_yellow       # Jogada 2: Amarelo (10)
+    li    t0, 2
+    beq   s5, t0, input_green        # Jogada 3: Verde (00)
+    li    t0, 3
+    beq   s5, t0, input_blue         # Jogada 4: Azul (01)
+    li    t0, 4
+    beq   s5, t0, input_red          # Jogada 5: Vermelho (11)
+    li    t0, 5
+    beq   s5, t0, input_yellow       # Jogada 6: Amarelo (10)
+    li    t0, 6
+    beq   s5, t0, input_green        # Jogada 7: Azul (00)
+    li    t0, 7
+    beq   s5, t0, input_blue         # Jogada 8: Verde (01)
+    li    t0, 8
+    j     DEFEAT                     # Entrada inválida
+
+input_red:
+    li    t5, 0b11                   # Vermelho
+    addi  s5, s5, 1                  
+    j     ST_EVALUATE
+
+input_yellow:
+    li    t5, 0b10                   # Amarelo
+    addi  s5, s5, 1
+    j     ST_EVALUATE
+
+input_blue:
+    li    t5, 0b00                   # Azul
+    addi  s5, s5, 1
+    j     ST_EVALUATE
+
+input_green:
+    li    t5, 0b01                   # Verde
+    addi  s5, s5, 1
+    j     ST_EVALUATE
+
+
+  # li    t5, 0b11                    # Simula apertar o vermelho (11) - CORRETO 
   # li    t5, 0b01                    # Simula apertar o verde    (01) - INCORRETO
-    li    s4, 0                       # Simula uma sequência de apenas um led (para teste)
-    j    ST_EVALUATE
+  # li    s4, 0                       # Simula uma sequência de apenas um led (para teste)
+  #  j    ST_EVALUATE
     
 
-ST_ADD_COLOR: # Para o modo mando eu
+ST_ADD_COLOR: # Label a ser utilizada no modo mando eu
+
 
 
 ST_EVALUATE:
@@ -99,7 +173,7 @@ ST_EVALUATE:
     # Atualiza contador e verifica fim da sequência
     addi  t5, t6, 1                   # Próxima posição
     beq   t6, s4, VICTORY             # Se todas entradas corretas → vitória
-    j     prng_generation             # ST_PLAYER_INPUT             # Repete para próxima entrada
+    j     aval_sec_size               # Repete para próxima entrada
 
    #slli s3, s3, 1   Habilita próximo led se o jogador acertar a jogada
 
