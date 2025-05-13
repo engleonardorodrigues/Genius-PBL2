414ed4990f259c833555db588d6d90c31585349d
Leonardo Rodrigues <leonardo.rodrigues7@yahoo.com.br>
2025-05-13 15:04:35 -0300

    Padronizando a formatação das labels e rotinas

--- software/lab_genius_asm.s
+++ software/lab_genius_asm.s
@@ -13,28 +13,28 @@ genius_game:
 
 ST_IDLE:                             # Estado inicial de configurações etc
 
-###### Definindo condição inicial dos registradores #####  (Reset ou estado de Vitória ou derrota)
+###### Definindo condição inicial dos registradores #####  (Caso seja iniciado um novo jogo)
  
-    addi s1, zero, 0
-    addi s2, zero, 0
-    addi s3, zero, 0
-    addi s4, zero, 0
-    addi s5, zero, 0
-    addi s6, zero, 0
-    addi s7, zero, 0
+    li s1, 0
+    li s2, 0
+    li s3, 0
+    li s4, 0
+    li s5, 0
+    li s6, 0
+    li s7, 0
 
 ###### Configurações do Game #####
 
-    li   s0,  0b0000                 # Modo = Siga (bit0 = 0), Velocidade = Lenta (bit1 = 0) Dificuldade = (bit2 e 3 = 0)  
+    li     s0,  0b0000                 # Modo = Siga (bit0 = 0), Velocidade = Lenta (bit1 = 0) Dificuldade = (bit2 e 3 = 0)  
 
 ###### Aguarda START para iniciar o jogo #####
 
-    li   t0,  START_BIT              # Aguarda botão START ser pressionado
+    li     t0,  START_BIT              # Aguarda botão START ser pressionado
 
     # addi t1,  t1, 1 #(Seed)        # Seed temporária (substituir por lógica adequada)
 
-    beqz t0,  ST_GEN                 # Se start pressionado, vai para o estado de gerar a sequência
-    j         ST_IDLE                # Se não, continua no estado de idle
+    beqz   t0,  ST_GEN                 # Se start pressionado, vai para o estado de gerar a sequência
+    j           ST_IDLE                # Se não, continua no estado de idle
 
 ST_GEN:
 
@@ -55,169 +55,125 @@ prng_generation:
 
     addi   s3, s3, 1 
     
-#    j      ST_SHOW_LEDS              # Vai mostrar o LED atual
-
-#aval_sec_size:                        # incrementa contagem de geração aleatória
-    
-#    blt    t3, s4, prng_generation   # Avalia se sequência foi concluida (8 fácil, 16 médio, 32 dificil)
-#    j      VICTORY
 
 ST_SHOW_LEDS:
+      
+    li   s7, 0                           # Reseta s7 (Apaga todos os leds)
+    li   s6, 0                           # Desabilita pino (Enable LED) 
+   
+    blt  t0, s3, ADD_LED                 # Verifica se ainda há cores para mostrar
+    j            ST_PLAYER_INPUT         # Quando terminar, vai para entrada do jogador
     
-    # bnez s3, show_color              # Necessário para manter somente 1 bit enable acionado por vez
-    # addi s3, s3, 1                   # Aciona o bit enable para ser possível visualizar o LED no jogo
-
-show_color:
+ADD_LED:
     
-    # Extrai os 2 bits da cor atual (t2) e mapeia para 4 bits em s7
-    
-    li   s7, 0                   # Reseta s7 (Apaga todos os leds)
-    li   s6, 0                   # Desabilita pino (Enable LED)
-    
-    #beq  t0, s3, show_sequence_color
-
-verifica_led: 
-    
-    #li t0, 0
-    
-    blt  t0, s3, add_led      # Verifica se ainda há cores para mostrar
-    j    reset_t0# ST_PLAYER_INPUT      # Quando terminar, vai para entrada do jogador
-    
-add_led:
-    # Calcula o deslocamento baseado no índice t0
-    addi t2, s1, 0
-    #and t2,  
-    sll t2, s1, t0
-    andi t2, t2, 0b11  
-    #addi t4, t4, 2
-    #sll t6, t2, t0
-    #andi t2, t2, 0b11 
-
-    #slli t2, t0, 2
-    #slli t4, t0, 2            # t4 = t0 * 2 (cada cor usa 2 bits)
-    #sll  t2, s1, t4           # Desloca s1 para direita para alinhar os bits
-    #andi t2, t2, 0b11         # Isola os 2 bits menos significativos
-    
-    addi t0, t0, 1            # Incrementa contador de posição
-    j    show_sequence_color  # Mostra a cor extraída
-
-show_sequence_color:
+    addi t2, s1, 0                       # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                      # Rotaciona os bits de acordo com t0
+    andi t2, t2, 0b11                    # Aplica a máscara ao conjunto de bits
+    addi t0, t0, 1                       # Incrementa contador de posição
 
     # Verifica qual cor está em t2 e configura os bits correspondentes
-    
 
-    beqz t2,     set_blue        # Se cor = 00 (Blue)
+    beqz t2,     SHOW_BLUE_LED        # Se cor = 00 (Blue)
     li   t5, 1
-    beq  t2, t5, set_green       # Se cor = 01 (Green)
+    beq  t2, t5, SHOW_GREEN_LED       # Se cor = 01 (Green)
     li   t5, 2
-    beq  t2, t5, set_yellow      # Se cor = 10 (Yellow)
+    beq  t2, t5, SHOW_YELLOW_LED      # Se cor = 10 (Yellow)
     li   t5, 3
-    beq  t2, t5, set_red         # Se cor = 11 (Red)
+    beq  t2, t5, SHOW_RED_LED         # Se cor = 11 (Red)
     
-set_green:
-    ori  s7, s7, 0b0001          # Verde      [1:0] = 01 -> saída 0001 (bit 0)
-    li   s6, 1                   # Enable_Led
 
-   ##slli s3, s3, 1               # Rotação a esquerda de um bit
-    #addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
+SHOW_GREEN_LED:
     
-    j    ST_PLAYER_INPUT
-
-set_blue:
-    ori  s7, s7, 0b0010          # Azul       [2:1] = 00 -> saída 0010 (bit 1)
-    li   s6, 1                   # Enable_Led
-
-   ##slli s3, s3, 1               # Rotação a esquerda de um bit
-    #addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
-
-    j    ST_PLAYER_INPUT
-
-set_red:
-    ori  s7, s7, 0b0100          # Vermelho   [3:2] = 11 -> saída 0100 (bit 2)
-    li   s6, 1                   # Enable_Led
-    
-   ##slli s3, s3, 1               # Rotação a esquerda de um bit
-    #addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
-    j    verifica_led
-    #j    ST_PLAYER_INPUT
-
-set_yellow:
-    ori  s7, s7, 0b1000          # Amarelo    [4:3] = 10 -> saída 1000 (bit 3)
-    li   s6, 1                   # Enable_Led
-    #addi s3, s3, 1 
-    j    verifica_led
-    #andi s6, s6, delay_led      (Implementar)
-
-    # Registrando que um led foi aceso
-   ##slli s3, s3, 1               # Rotação a esquerda de um bit
-    #addi s3, s3, 1               # Insere um novo bit para indicar que mais uma cor foi inserida na sequência
-
-    #j    ST_PLAYER_INPUT
+    ori  s7, s7, 0b0001              # Verde [1:0] = 01 -> saída 0001 (bit 0)
+    li   s6, 1                       # Enable_Led
+    j    ST_SHOW_LEDS                # Vai para o estado de mostrar os leds
 
 
-#delay_led:                       # ADAPTAR O TEMPO DO LED DE ACORDO COM A VELOCIDADE (2s lento, 1s rápido)
+SHOW_BLUE_LED:
+
+    ori  s7, s7, 0b0010              # Azul [2:1] = 00 -> saída 0010 (bit 1)
+    li   s6, 1                       # Enable_Led
+    j    ST_SHOW_LEDS                # Vai para o estado de mostrar os leds
+
+
+SHOW_RED_LED:
+
+    ori  s7, s7, 0b0100              # Vermelho [3:2] = 11 -> saída 0100 (bit 2)
+    li   s6, 1                       # Enable_Led
+    j    ST_SHOW_LEDS                # Vai para o estado de mostrar os leds
+
+
+SHOW_YELLOW_LED:
+
+    ori  s7, s7, 0b1000              # Amarelo [4:3] = 10 -> saída 1000 (bit 3)
+    li   s6, 1                       # Enable_Led
+    j    ST_SHOW_LEDS                # Vai para o estado de mostrar os leds
+ 
+
+#DELAY_LED:                      # ADAPTAR O TEMPO DO LED DE ACORDO COM A VELOCIDADE (2s lento, 1s rápido)
     
 #    li t1, 2000
 
 #    addi t0, t0, 1  
 #    blt  t0, t1, delay_led 
 
-reset_t0:
+#reset_t0:
 
-li t0, 0
+#li t0, 0
+#li s5, 0
 
 ST_PLAYER_INPUT:
 
-   li    t5, 0b11                   # Simula apertar o vermelho (11) - CORRETO 
-#  li    t5, 0b01                   # Simula apertar o verde    (01) - INCORRETO
- # li    s4, 3                      # Simula uma sequência de apenas um led (para teste)
- #  j    ST_EVALUATE
+    li    t0, 0
+    li    s5, 0
+    li    t5, 0b11                   # Simula apertar o vermelho (11) - CORRETO 
 
     # Simula a entrada do jogador (Implementar no simulador futuramente)
-    beqz  s5, input_red              # Jogada 1: Vermelho (11)
+
+    beqz  s5,     INPUT_RED          # Jogada 1: Vermelho (11)
     li    t0, 1
-    beq   s5, t0, input_yellow       # Jogada 2: Amarelo (10)
+    beq   s5, t0, INPUT_YELLOW       # Jogada 2: Amarelo (10)
     li    t0, 2
-    beq   s5, t0, input_green        # Jogada 3: Verde (00)
+    beq   s5, t0, INPUT_GREEN        # Jogada 3: Verde (00)
     li    t0, 3
-    beq   s5, t0, input_blue         # Jogada 4: Azul (01)
+    beq   s5, t0, INPUT_BLUE         # Jogada 4: Azul (01)
     li    t0, 4
-    beq   s5, t0, input_red          # Jogada 5: Vermelho (11)
+    beq   s5, t0, INPUT_RED          # Jogada 5: Vermelho (11)
     li    t0, 5
-    beq   s5, t0, input_yellow       # Jogada 6: Amarelo (10)
+    beq   s5, t0, INPUT_YELLOW       # Jogada 6: Amarelo (10)
     li    t0, 6
-    beq   s5, t0, input_green        # Jogada 7: Azul (00)
+    beq   s5, t0, INPUT_GREEN        # Jogada 7: Azul (00)
     li    t0, 7
-    beq   s5, t0, input_blue         # Jogada 8: Verde (01)
+    beq   s5, t0, INPUT_GREEN        # Jogada 8: Verde (01)
     li    t0, 8
     j     DEFEAT                     # Entrada inválida
 
-input_red:
-    li    t5, 0b11                   # Vermelho
-    addi  s5, s5, 1
-    #li    t0, 0
-    #addi  s3, s3, 1                   
-    j     ST_EVALUATE
 
-input_yellow:
-    li    t5, 0b10                   # Amarelo
+# Aciona a saída do respectivo LED
+
+INPUT_RED:
+
+    li    t5, 0b11                   # Seta cor Vermelha
+    addi  s5, s5, 1                  # Registra jogada do jogador
+    j     ST_EVALUATE                # Vai para o estado de avaliar Vitória ou derrota
+
+INPUT_YELLOW:
+
+    li    t5, 0b10                   # Seta cor Amarela
     addi  s5, s5, 1                  # Registra a jogada do player
-    #li    t0, 0
-    #addi  s3, s3, 1   
-    j     ST_EVALUATE
+    j     ST_EVALUATE                # Vai para o estado de avaliar Vitória ou derrota
 
-input_blue:
-    li    t5, 0b00                   # Azul
+INPUT_BLUE:
+
+    li    t5, 0b00                   # Seta cor Azul
     addi  s5, s5, 1                  # Registra a jogada do player
-    #addi  s3, s3, 1   
-    j     ST_EVALUATE
+    j     ST_EVALUATE                # Vai para o estado de avaliar Vitória ou derrota
 
-input_green:
-    li    t5, 0b01                   # Verde
+INPUT_GREEN:
+
+    li    t5, 0b01                   # Seta cor Verde
     addi  s5, s5, 1                  # Registra a jogada do player
-    #addi  s3, s3, 1   
-    j     ST_EVALUATE
-
+    j     ST_EVALUATE                # Vai para o estado de avaliar Vitória ou derrota
 
 
 ST_ADD_COLOR: # Label a ser utilizada no modo mando eu
@@ -228,17 +184,25 @@ ST_EVALUATE:
    
     # implementar verificação de modo de jogo (mando eu ou siga eu) 
     
-    bne   t5, t2, DEFEAT              # Entrada errada → derrota
+    addi t2, s1, 0                            # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                           # Rotaciona os bits de acordo com t0
+    andi t2, t2, 0b11                         # Aplica a máscara ao conjunto de bits
 
-    # Atualiza contador e verifica fim da sequência
-    #addi  t5, t6, 1                   # Próxima posição
-    #beq   t6, s4, VICTORY             # Se todas entradas corretas → vitória
-    #j     aval_sec_size              # Repete para próxima entrada
-    blt    t3, s4, prng_generation    # Avalia se sequência foi concluida (8 fácil, 16 médio, 32 dificil)
-    j      VICTORY
+    beq   t5, t2, ST_INPUT_PLAYER_OR_PRNG     # Entrada errada → derrota
+  # bne   t5, t2, ST_END #DEFEAT             # Entrada errada → derrota
+  # blt   t3, s4, prng_generation             # Avalia se sequência foi concluida (8 fácil, 16 médio, 32 dificil)
+  # j     VICTORY
+    j     ST_END
 
 
-   #slli s3, s3, 1   Habilita próximo led se o jogador acertar a jogada
+ST_INPUT_PLAYER_OR_PRNG:
+
+   beq s5, s3, prng_generation
+   j   ST_PLAYER_INPUT
+
+ST_END:
+
+bne   t5, t2, DEFEAT  
 
 VICTORY: 
     li s6, 2 # configura o led de vitória
