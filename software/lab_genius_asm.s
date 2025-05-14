a9d2dd17fd8ebc0f9c79bb38ba7958e01e205178
Leonardo Rodrigues <leonardo.rodrigues7@yahoo.com.br>
2025-05-14 14:19:05 -0300

    Finish Genius Game

--- software/lab_genius_asm.s
+++ software/lab_genius_asm.s
@@ -20,33 +20,33 @@ ST_IDLE:
 #      (Caso seja iniciado um novo jogo)                
 #########################################################  
  
-    li s1, 0                                # reset inicial
-    li s2, 0                                # reset inicial
-    li s3, 0                                # reset inicial 
-    li s4, 0                                # reset inicial 
-    li s5, 0                                # reset inicial 
-    li s6, 0                                # reset inicial
-    li s7, 0                                # reset inicial
+    li s1, 0                                                # reset inicial
+    li s2, 0                                                # reset inicial
+    li s3, 0                                                # reset inicial 
+    li s4, 0                                                # reset inicial 
+    li s5, 0                                                # reset inicial 
+    li s6, 0                                                # reset inicial
+    li s7, 0                                                # reset inicial
 
 #########################################################
 #   Configurações do Game                                                                     
 #########################################################
 
-    li  s0, 0                              # Reseta o registrador de configuração do jogo
-    ori s0, s0, GAME_MODE                  # Modo        = Siga  (bit0 = 0) 
-    ori s0, s0, SPEED_GAME                 # Velocidade  = Lenta (bit1 = 0)
-    ori s0, s0, DIFFICULTY                 # Dificuldade = Fácil (bit2 e 3 = 0)         
+    li  s0, 0                                               # Reseta o registrador de configuração do jogo
+    ori s0, s0, GAME_MODE                                   # Modo        = Siga  (bit0 = 0) 
+    ori s0, s0, SPEED_GAME                                  # Velocidade  = Lenta (bit1 = 0)
+    ori s0, s0, DIFFICULTY                                  # Dificuldade = Fácil (bit2 e 3 = 0)         
   
 #########################################################
 #   Aguarda START para iniciar o jogo                   
 #########################################################
 
-    li     t0,  START_BIT                    # Aguarda botão START ser pressionado
+    li     t0,  START_BIT                                   # Aguarda botão START ser pressionado
 
-    addi   t1,  t1, 0                        # Contador responsável por gerar a SEED
+    addi   t1,  t1, 0                                       # Contador responsável por gerar a SEED
 
-    beqz   t0,  ST_GEN                       # Se start pressionado, vai para ST_GEN
-    j           ST_IDLE                      # Se não, continua no estado de IDLE
+    beqz   t0,  ST_GEN                                      # Se start pressionado, vai para ST_GEN
+    j           ST_IDLE                                     # Se não, continua no estado de IDLE
 
 #########################################################
 #   ST_GEN:Estado para geração do número aleatório                                                                                  
@@ -54,9 +54,9 @@ ST_IDLE:
 
 ST_GEN:
 
-    li     s1, 0                             # s1: led_sequence_reg1 vai armazenar os 16 bits gerados (cores
-    li     t3, 0                             # contador de quantas iterações já fizemos
-    li     s4, 8                             # s4: Sequence_size (define o tamanho da sequência de acordo com o nível de dificuldade)	
+    li     s1, 0                                            # s1: led_sequence_reg1 vai armazenar os 16 bits gerados (cores
+    li     t3, 0                                            # contador de quantas iterações já fizemos
+    li     s4, 8                                            # s4: Sequence_size (define o tamanho da sequência de acordo com o nível de dificuldade)	
 
 #########################################################
 #   PRNG: Gerador de números aleatórios                                                                                    
@@ -64,15 +64,15 @@ ST_GEN:
 
 PRNG:
     
-    addi t1, t1, 3                           # Gera a seed
-    andi t2, t1, 0b11                        # Mascara para obter 2 bits (0-3)
+    addi t1, t1, 3                                          # Gera a seed
+    andi t2, t1, 0b11                                       # Mascara para obter 2 bits (0-3)
 
-    slli t4, t3, 1                           # Desloca os dois bits para esquerda
-    sll  t5, t2, t4                          # Desloca os bits da nova cor para a posição atual
-    or   s1, s1, t5                          # Adiciona ao registrador s1
+    slli t4, t3, 1                                          # Desloca os dois bits para esquerda
+    sll  t5, t2, t4                                         # Desloca os bits da nova cor para a posição atual
+    or   s1, s1, t5                                         # Adiciona ao registrador s1
 
-    #addi t3, t3, 1 
-    addi s3, s3, 1                           # Contador que indica quantas rodadas foram feitas
+    li t3, 0                                                # Zera contator t3
+    addi s3, s3, 1                                          # Contador que indica quantas rodadas foram feitas
     
 #########################################################
 #   Verifica entrada do jogador ou exibe sequência de LEDs    
@@ -80,11 +80,11 @@ PRNG:
 
 ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED:
       
-    li   s7, 0                               # Reseta s7 (Apaga todos os leds)
-    li   s6, 0                               # Desabilita pino (Enable LED) 
+    li   s7, 0                                              # Reseta s7 (Apaga todos os leds)
+    li   s6, 0                                              # Desabilita pino (Enable LED) 
    
-    blt  t0, s3, ST_SHOW_LEDS                # Verifica se ainda há cores para mostrar
-    j            ST_PLAYER_INPUT             # Quando terminar, vai para entrada do jogador
+    blt  t0, s3, ST_SHOW_LEDS                               # Verifica se ainda há cores para mostrar
+    j            ST_PLAYER_INPUT                            # Quando terminar, vai para entrada do jogador
 
 #########################################################
 #   Exibe a sequência de LEDs para o jogador memorizar               
@@ -92,20 +92,20 @@ ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED:
 
 ST_SHOW_LEDS:
     
-    addi t2, s1, 0                           # Copia o registrador s1 para o t2
-    sll  t2, s1, t0                          # Rotaciona os bits de acordo com t0
-    andi t2, t2, 0b11                        # Aplica a máscara ao conjunto de bits
-    addi t0, t0, 1                           # Incrementa contador de posição
+    addi t2, s1, 0                                          # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                                         # Rotaciona os bits de acordo com t0
+    andi t2, t2, 0b11                                       # Aplica a máscara ao conjunto de bits
+    addi t0, t0, 1                                          # Incrementa contador de posição
 
     # Verifica qual cor está em t2 e configura os bits correspondentes
 
-    beqz t2,     SHOW_BLUE_LED               # Se cor = 00 (Blue)
+    beqz t2,     SHOW_BLUE_LED                              # Se cor = 00 (Blue)
     li   t5, 1                              
-    beq  t2, t5, SHOW_GREEN_LED              # Se cor = 01 (Green)
+    beq  t2, t5, SHOW_GREEN_LED                             # Se cor = 01 (Green)
     li   t5, 2
-    beq  t2, t5, SHOW_YELLOW_LED             # Se cor = 10 (Yellow)
+    beq  t2, t5, SHOW_YELLOW_LED                            # Se cor = 10 (Yellow)
     li   t5, 3
-    beq  t2, t5, SHOW_RED_LED                # Se cor = 11 (Red)
+    beq  t2, t5, SHOW_RED_LED                               # Se cor = 11 (Red)
     
 #########################################################
 #   Acende o LED verde             
@@ -113,9 +113,9 @@ ST_SHOW_LEDS:
 
 SHOW_GREEN_LED:
     
-    ori  s7, s7, 0b0001                      # Verde [1:0] = 01 -> saída 0001 (bit 0)
-    li   s6, 1                               # Enable_Led
-    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED  # Vai para o estado de mostrar os leds
+    ori  s7, s7, 0b0001                                     # Verde [1:0] = 01 -> saída 0001 (bit 0)
+    li   s6, 1                                              # Enable_Led
+    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED                 # Vai para o estado de mostrar os leds
 
 #########################################################
 #   Acende o LED azul             
@@ -123,9 +123,9 @@ SHOW_GREEN_LED:
 
 SHOW_BLUE_LED:
 
-    ori  s7, s7, 0b0010                      # Azul [2:1] = 00 -> saída 0010 (bit 1)
-    li   s6, 1                               # Enable_Led
-    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED  # Vai para o estado de mostrar os leds
+    ori  s7, s7, 0b0010                                     # Azul [2:1] = 00 -> saída 0010 (bit 1)
+    li   s6, 1                                              # Enable_Led
+    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED                 # Vai para o estado de mostrar os leds
 
 #########################################################
 #   Acende o LED vermelho            
@@ -133,9 +133,9 @@ SHOW_BLUE_LED:
 
 SHOW_RED_LED:
 
-    ori  s7, s7, 0b0100                      # Vermelho [3:2] = 11 -> saída 0100 (bit 2)
-    li   s6, 1                               # Enable_Led
-    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED  # Vai para o estado de mostrar os leds
+    ori  s7, s7, 0b0100                                     # Vermelho [3:2] = 11 -> saída 0100 (bit 2)
+    li   s6, 1                                              # Enable_Led
+    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED                 # Vai para o estado de mostrar os leds
 
 #########################################################
 #   Acende o LED amarelo             
@@ -143,9 +143,9 @@ SHOW_RED_LED:
 
 SHOW_YELLOW_LED:
 
-    ori  s7, s7, 0b1000                      # Amarelo [4:3] = 10 -> saída 1000 (bit 3)
-    li   s6, 1                               # Enable_Led
-    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED  # Vai para o estado de mostrar os leds
+    ori  s7, s7, 0b1000                                     # Amarelo [4:3] = 10 -> saída 1000 (bit 3)
+    li   s6, 1                                              # Enable_Led
+    j    ST_VERIFY_INPUT_PLAYER_OR_SHOW_LED                 # Vai para o estado de mostrar os leds
  
 #########################################################
 #   Aguarda a entrada do jogador e verifica se está correta            
@@ -153,41 +153,45 @@ SHOW_YELLOW_LED:
 
 ST_PLAYER_INPUT:
 
-    li    t0, 0                              # Reseta o contador de jogadas do jogador
-    li    s5, 0                              # Reseta registrador 
+    li    t0, 0                                             # Reseta o contador de jogadas do jogador
+    li    s5, 0                                             # Reseta registrador 
 
     # Simula a entrada do jogador 
     # (Implementar no simulador futuramente)
 
-    li   t5,     0b11                        # Simula jogador apertando botão da cor amarela
-    #addi t3, t3, 1                           # Incrementa contador de geração aleatória
-    addi t2, s1, 0                           # Copia o registrador s1 para o t2
-    sll  t2, s1, t0                          # Rotaciona os bits de acordo com t0
-    andi t2, t2, 0b11                        # Aplica a máscara ao conjunto de bits
-    beq  t5, t2  ST_EVALUATE                 # Jogada 1: Vermelho (11)
+    li   t5,     0b11                                       # Simula jogador apertando botão da cor amarela
+    addi t3, t3, 1                                          # Incrementa contador de geração aleatória
+    addi t2, s1, 0                                          # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                                         # Rotaciona os bits de acordo com t0
+    #addi t6, t6, 2
+    andi t2, t2, 0b11                                       # Aplica a máscara ao conjunto de bits
+    beq  t5, t2  ST_EVALUATE                                # Jogada 1: Vermelho (11)
     
-    li   t5,     0b10                        # Simula jogador apertando botão da cor amarela  
-    #addi t3, t3, 1                           # Incrementa contador de geração aleatória
-    addi t2, s1, 0                           # Copia o registrador s1 para o t2
-    sll  t2, s1, t0                          # Rotaciona os bits de acordo com t0
-    andi t2, t2, 0b11                        # Aplica a máscara ao conjunto de bits
-    beq  t5, t2  ST_EVALUATE                 # Jogada 2: Amarelo (10)
+    li   t5,     0b10                                       # Simula jogador apertando botão da cor amarela  
+    addi t3, t3, 1                                          # Incrementa contador de geração aleatória
+    addi t2, s1, 0                                          # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                                         # Rotaciona os bits de acordo com t0
+    #addi t6, t6, 2
+    andi t2, t2, 0b11                                       # Aplica a máscara ao conjunto de bits
+    beq  t5, t2  ST_EVALUATE                                # Jogada 2: Amarelo (10)
 
-    li   t5,     0b01                        # Simula jogador apertando botão da cor verde
-    #addi t3, t3, 1                           # Incrementa contador de geração aleatória
-    addi t2, s1, 0                           # Copia o registrador s1 para o t2
-    sll  t2, s1, t0                          # Rotaciona os bits de acordo com t0
-    andi t2, t2, 0b11                        # Aplica a máscara ao conjunto de bits   
-    addi t3, t3, 1                           # Incrementa contador de geração aleatória
-    beq  t5, t2  ST_EVALUATE                 # Jogada 3: Verde (00)
+    li   t5,     0b01                                       # Simula jogador apertando botão da cor verde
+    addi t3, t3, 1                                          # Incrementa contador de geração aleatória
+    addi t2, s1, 0                                          # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                                         # Rotaciona os bits de acordo com t0
+    #addi t6, t6, 2
+    andi t2, t2, 0b11                                       # Aplica a máscara ao conjunto de bits   
+    addi t3, t3, 1                                          # Incrementa contador de geração aleatória
+    beq  t5, t2  ST_EVALUATE                                # Jogada 3: Verde (00)
    
-    li   t5,     0b00                        # Simula jogador apertando botão da cor azul 
-    #addi t3, t3, 1                           # Incrementa contador de geração aleatória
-    addi t2, s1, 0                           # Copia o registrador s1 para o t2
-    sll  t2, s1, t0                          # Rotaciona os bits de acordo com t0
-    andi t2, t2, 0b11                        # Aplica a máscara ao conjunto de bits       
-    addi t3, t3, 1                           # Incrementa contador de geração aleatória 
-    beq  t5, t2  ST_EVALUATE                 # Jogada 4: Azul (01)
+    li   t5,     0b00                                       # Simula jogador apertando botão da cor azul 
+    addi t3, t3, 1                                          # Incrementa contador de geração aleatória
+    addi t2, s1, 0                                          # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                                         # Rotaciona os bits de acordo com t0
+    #addi t6, t6, 2
+    andi t2, t2, 0b11                                       # Aplica a máscara ao conjunto de bits       
+    addi t3, t3, 1                                          # Incrementa contador de geração aleatória 
+    beq  t5, t2  ST_EVALUATE                                # Jogada 4: Azul (01)
 
 
 #########################################################
@@ -197,17 +201,20 @@ ST_PLAYER_INPUT:
 
 ST_EVALUATE:
 
-    #addi t3, t3, 1                           # Incrementa contador de geração aleatória   
-    addi t2, s1, 0                           # Copia o registrador s1 para o t2
-    sll  t2, s1, t0                          # Rotaciona os bits de acordo com t0
-    andi t2, t2, 0b11                        # Aplica a máscara ao conjunto de bits
+    #addi s3, s3, 1                                         # Incrementa contador de geração aleatória   
+    addi t2, s1, 0                                          # Copia o registrador s1 para o t2
+    sll  t2, s1, t0                                         # Rotaciona os bits de acordo com t0
+    andi t2, t2, 0b11                                       # Aplica a máscara ao conjunto de bits
 
-    addi s5, s1, 0                           # Registra jogada do jogador
-    addi t5, s5, 0                           # Copia o registrador s1 para o t2
-    sll  t5, s5, t0                          # Rotaciona os bits de acordo com t0
-    andi t5, t5, 0b11                        # Aplica a máscara ao conjunto de bits
+    addi s5, s1, 0                                          # Registra jogada do jogador
+    addi t5, s5, 0                                          # Copia o registrador s1 para o t2
+    sll  t5, s5, t0                                         # Rotaciona os bits de acordo com t0
+    andi t5, t5, 0b11                                       # Aplica a máscara ao conjunto de bits
 
-    beq  t5, t2, ST_INPUT_PLAYER_OR_PRNG    # Verificar se é a vez do usuário jogar ou se é para gerar um novo aletório
+    #addi t0, t0, 2
+
+    beq  t5, t2, ST_INPUT_PLAYER_OR_PRNG                    # Verificar se é a vez do usuário jogar ou se é para gerar um novo aletório
+
     j    ST_END
 
 #########################################################
@@ -217,9 +224,9 @@ ST_EVALUATE:
 ST_INPUT_PLAYER_OR_PRNG:
 
    #addi t3, t3, 1 
-   bne t3, s3, PRNG                           # Se o contador de jogadas do jogador for igual ao contador de jogadas do PRNG, vai para o estado de gerar a sequência
+   beq t3, s3, PRNG                                         # Se o contador de jogadas do jogador for igual ao contador de jogadas do PRNG, vai para o estado de gerar a sequência
    #addi t3, t3, 1 
-   j           ST_PLAYER_INPUT                # Se não, continua no estado de entrada do jogador
+   j           ST_PLAYER_INPUT                              # Se não, continua no estado de entrada do jogador
 
 #########################################################
 #   Estado que verifica se o jogador venceu ou perdeu         
@@ -227,14 +234,14 @@ ST_INPUT_PLAYER_OR_PRNG:
 
 ST_END:
 
-   bne t5, t2, DEFEAT                          # Entrada errada → derrota
+   bne t5, t2, DEFEAT                                       # Entrada errada → derrota
 
 VICTORY: 
 
-   li s6, 2                                   # Configura o led de vitória
-   j ST_IDLE                                  # Retorna para o estado de IDLE
+   li s6, 2                                                 # Configura o led de vitória
+   j ST_IDLE                                                # Retorna para o estado de IDLE
 
 DEFEAT: 
 
-   li s6, 2                                   # configura o led de derrota
-   j ST_IDLE                                  # Retorna para o estado de IDLE
+   li s6, 2                                                 # configura o led de derrota
+   j ST_IDLE                                                # Retorna para o estado de IDLE
